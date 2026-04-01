25d86253966a5347c34b9e0bca8692fc52357293
SAMAR888-oss <your_gunchaara@gmail.com>
2026-04-01 09:52:15 +0530

    UC2: Add Passenger Bogies to Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,26 +1,45 @@
+import java.util.ArrayList;
+
 public class TrainConsistManagementApp {
 
-    // Train name
     String trainName;
+    ArrayList<String> bogies;
 
     // Constructor
     public TrainConsistManagementApp(String trainName) {
         this.trainName = trainName;
+        this.bogies = new ArrayList<>();
     }
 
-    // Method to display consist summary
+    // Add bogie
+    public void addBogie(String bogieId) {
+        bogies.add(bogieId);
+    }
+
+    // Display summary
     public void displayConsistSummary() {
         System.out.println("Train: " + trainName);
         System.out.println("Consist Summary:");
-        System.out.println("No bogies added yet");
+
+        if (bogies.isEmpty()) {
+            System.out.println("No bogies added yet");
+        } else {
+            for (String bogie : bogies) {
+                System.out.println("Bogie: " + bogie);
+            }
+        }
     }
 
     public static void main(String[] args) {
 
-        // Initialize train
         TrainConsistManagementApp train = new TrainConsistManagementApp("Express");
 
-        // Display summary
+        // Add bogies
+        train.addBogie("B1");
+        train.addBogie("B2");
+        train.addBogie("B3");
+
+        // Display
         train.displayConsistSummary();
     }
 }
