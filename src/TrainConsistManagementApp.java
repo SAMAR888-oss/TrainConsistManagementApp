21766a14a010b5bfd404fc40a5d648b5bc9fff1f
SAMAR888-oss <your_gunchaara@gmail.com>
2026-04-01 09:53:52 +0530

    UC3: Track Unique Bogie IDs using Set

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,19 +1,23 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TrainConsistManagementApp {
 
     String trainName;
-    ArrayList<String> bogies;
+    HashSet<String> bogies;
 
     // Constructor
     public TrainConsistManagementApp(String trainName) {
         this.trainName = trainName;
-        this.bogies = new ArrayList<>();
+        this.bogies = new HashSet<>();
     }
 
-    // Add bogie
+    // Add bogie (no duplicates)
     public void addBogie(String bogieId) {
-        bogies.add(bogieId);
+        if (bogies.add(bogieId)) {
+            System.out.println(bogieId + " added successfully");
+        } else {
+            System.out.println("Duplicate bogie not allowed: " + bogieId);
+        }
     }
 
     // Display summary
@@ -34,10 +38,10 @@ public class TrainConsistManagementApp {
 
         TrainConsistManagementApp train = new TrainConsistManagementApp("Express");
 
-        // Add bogies
+        // Add bogies (with duplicate)
         train.addBogie("B1");
         train.addBogie("B2");
-        train.addBogie("B3");
+        train.addBogie("B1"); // duplicate
 
         // Display
         train.displayConsistSummary();
