fe291f6b8c0cd0e903624ae595d8bd29cff5cf78
SAMAR888-oss <your_gunchaara@gmail.com>
2026-04-01 10:21:00 +0530

    UC11: Validate Train ID and Cargo Codes

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,36 +1,20 @@
-import java.util.*;
+import java.util.regex.*;
 
 public class TrainConsistManagementApp {
 
-    String trainName;
-    HashMap<String, Integer> bogieCapacity;
-
-    public TrainConsistManagementApp(String trainName) {
-        this.trainName = trainName;
-        this.bogieCapacity = new HashMap<>();
+    public boolean validateTrainId(String trainId) {
+        return trainId.matches("TR\\d{3}");
     }
 
-    public void addBogie(String bogieId, int capacity) {
-        bogieCapacity.put(bogieId, capacity);
-    }
-
-    public void countTotalSeats() {
-
-        int total = bogieCapacity.values()
-                .stream()
-                .reduce(0, Integer::sum);
-
-        System.out.println("Total Seats: " + total);
+    public boolean validateCargoCode(String code) {
+        return code.matches("CG-[A-Z]{2}\\d{2}");
     }
 
     public static void main(String[] args) {
 
-        TrainConsistManagementApp train = new TrainConsistManagementApp("Express");
+        TrainConsistManagementApp app = new TrainConsistManagementApp();
 
-        train.addBogie("B1", 50);
-        train.addBogie("B2", 60);
-        train.addBogie("B3", 40);
-
-        train.countTotalSeats();
+        System.out.println("Train ID valid: " + app.validateTrainId("TR123"));
+        System.out.println("Cargo valid: " + app.validateCargoCode("CG-AB12"));
     }
 }
