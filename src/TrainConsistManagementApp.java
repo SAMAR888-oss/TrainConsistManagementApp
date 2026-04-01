a511aa3a16bd43dd5c8053bacde3449ba7ce57d2
SAMAR888-oss <your_gunchaara@gmail.com>
2026-04-01 10:13:05 +0530

    UC7: Sort Bogies by Capacity using Comparator

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,28 +1,30 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class TrainConsistManagementApp {
 
     String trainName;
     HashMap<String, Integer> bogieCapacity;
 
-    // Constructor
     public TrainConsistManagementApp(String trainName) {
         this.trainName = trainName;
         this.bogieCapacity = new HashMap<>();
     }
 
-    // Add bogie with capacity
     public void addBogie(String bogieId, int capacity) {
         bogieCapacity.put(bogieId, capacity);
     }
 
-    // Display summary
-    public void displayConsistSummary() {
-        System.out.println("Train: " + trainName);
-        System.out.println("Bogie Capacity Details:");
+    public void displaySortedBogies() {
 
-        for (String bogie : bogieCapacity.keySet()) {
-            System.out.println("Bogie: " + bogie + " → Capacity: " + bogieCapacity.get(bogie));
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(bogieCapacity.entrySet());
+
+        // Sort using Comparator (by capacity)
+        list.sort((a, b) -> a.getValue() - b.getValue());
+
+        System.out.println("Sorted Bogies by Capacity:");
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + " → " + entry.getValue());
         }
     }
 
@@ -30,12 +32,10 @@ public class TrainConsistManagementApp {
 
         TrainConsistManagementApp train = new TrainConsistManagementApp("Express");
 
-        // Add bogies with capacity
         train.addBogie("B1", 50);
         train.addBogie("B2", 60);
         train.addBogie("B3", 45);
 
-        // Display
-        train.displayConsistSummary();
+        train.displaySortedBogies();
     }
 }
